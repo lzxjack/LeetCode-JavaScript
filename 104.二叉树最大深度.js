b29cc69056801217f249c0b15ef32c5ef2fe73f1
Jack <965555169@qq.com>
2021-04-27 11:44:28 +0800

    update

--- 104.二叉树最大深度.js
+++ 104.二叉树最大深度.js
@@ -14,19 +14,24 @@
 //    15   7
 // 返回它的最大深度 3 。
 
-var maxDepth = function(root) {
-    // 定义深度的变量
-    let res = 0;
-    // n为二叉树，level层级
-    const dfs = (n, level) => {
-        if (!n) { return; }
-        // 子节点才刷新层级
-        if (!n.left && !n.right) {
-            res = Math.max(res, level);
-        }
-        dfs(n.left, level + 1);
-        dfs(n.right, level + 1);
-    }
-    dfs(root, 1);
-    return res;
+// var maxDepth = function(root) {
+//     // 定义深度的变量
+//     let res = 0;
+//     // n为二叉树，level层级
+//     const dfs = (n, level) => {
+//         if (!n) { return; }
+//         // 子节点才刷新层级
+//         if (!n.left && !n.right) {
+//             res = Math.max(res, level);
+//         }
+//         dfs(n.left, level + 1);
+//         dfs(n.right, level + 1);
+//     }
+//     dfs(root, 1);
+//     return res;
+// };
+
+const maxDepth = function (root) {
+    if (!root) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
