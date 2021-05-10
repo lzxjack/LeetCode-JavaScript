375a7e5eca67fff33cfdeafbb8e0ae0ca8b0fc96
Jack <965555169@qq.com>
2021-05-10 11:51:44 +0800

    update

--- 111.二叉树的最小深度.js
+++ 111.二叉树的最小深度.js
@@ -4,17 +4,28 @@
 
 // 说明：叶子节点是指没有子节点的节点。
 
-var minDepth = function(root) {
-    if (!root) { return 0; }
-    const q = [
-        [root, 1]
-    ];
-    while (q.length) {
-        const [n, l] = q.shift();
-        if (!n.left && !n.right) {
-            return l;
-        }
-        if (n.left) q.push([n.left, l + 1]);
-        if (n.right) q.push([n.right, l + 1]);
-    }
+// var minDepth = function(root) {
+//     if (!root) { return 0; }
+//     const q = [
+//         [root, 1]
+//     ];
+//     while (q.length) {
+//         const [n, l] = q.shift();
+//         if (!n.left && !n.right) {
+//             return l;
+//         }
+//         if (n.left) q.push([n.left, l + 1]);
+//         if (n.right) q.push([n.right, l + 1]);
+//     }
+// };
+
+const minDepth = root => {
+    // 终止条件：树空
+    if (!root) return 0;
+    // 左子树为空，返回右子树最小深度+1
+    if (!root.left) return minDepth(root.right) + 1;
+    // 右子树为空，返回左子树最小深度+1
+    if (!root.right) return minDepth(root.left) + 1;
+    // 返回当前树的最小深度
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
