493ce85df90d3b668c7654ded24162b877435c6d
Jack <965555169@qq.com>
2021-07-21 09:44:25 +0800

    update

--- 剑指Offer/58 - I. 翻转单词顺序.js
+++ 剑指Offer/58 - I. 翻转单词顺序.js
@@ -1,12 +1,3 @@
-// let a = '     ';
-// let arr = a.split(' ');
-// const res = [];
-// for (let i = arr.length - 1; i >= 0; i--) {
-//     if (arr[i]) res.push(arr[i]);
-// }
-
-// console.log(res.join(' '));
-
 const reverseWords = s => {
     const arr = s.split(' ');
     const res = [];
