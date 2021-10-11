4666e89810c0a73f22b88ed7d53e49b5e809fd08
Jack <965555169@qq.com>
2021-10-11 17:15:46 +0800

    update

---
+++ 栈/946. 验证栈序列.js
@@ -0,0 +1,13 @@
+const validateStackSequences = (pushed, popped) => {
+    const stack = [];
+    let index = 0;
+    const len = pushed.length;
+    for (let i = 0; i < len; i++) {
+        stack.push(pushed[i]);
+        while (popped[index] !== undefined && popped[index] === stack[stack.length - 1]) {
+            stack.pop();
+            index++;
+        }
+    }
+    return !stack.length;
+};
