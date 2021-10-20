63318a17a6f90baa298b4201081a50adae34b5a7
Jack <965555169@qq.com>
2021-10-20 09:05:15 +0800

    update

--- 双指针/415. 字符串相加.js
+++ 双指针/415. 字符串相加.js
@@ -2,17 +2,19 @@ const addStrings = (num1, num2) => {
     // 定义两指针分别指向两个数的尾
     let [i, j] = [num1.length - 1, num2.length - 1];
     // carry进位数
-    let [carry, res] = [0, []];
+    let carry = 0;
+    let res = [];
 
-    while (i >= 0 || j >= 0 || carry !== 0) {
+    while (i >= 0 || j >= 0 || carry) {
         // 判断指针是否到头，没到头就隐式转换成数字，到头就赋为0
         const c1 = i >= 0 ? num1[i] - '' : 0;
         const c2 = j >= 0 ? num2[j] - '' : 0;
         const sum = c1 + c2 + carry;
-        // 取余数，从队头放入res
-        res.unshift(sum % 10);
-        // 重新设置进位，'|0'可以取整
-        carry = (sum / 10) | 0;
+        // 个位数
+        const leave = sum % 10;
+        res.unshift(leave);
+        // 更新进位为十位数
+        carry = (sum - leave) / 10;
         i--;
         j--;
     }
@@ -20,5 +22,3 @@ const addStrings = (num1, num2) => {
     // 数组连接成字符串
     return res.join('');
 };
-
-addStrings('', '45');
