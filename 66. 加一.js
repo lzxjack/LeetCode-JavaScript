043e3995998f4cc3cded20a51393b3d5b32badb9
Jack <965555169@qq.com>
2021-10-21 08:45:24 +0800

    update

--- 66. 加一.js
+++ 66. 加一.js
@@ -1,16 +1,18 @@
-let plusOne = function (digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
+const plusOne = digits => {
+    const len = digits.length;
+    for (let i = len - 1; i >= 0; i--) {
         if (digits[i] < 9) {
+            // 当前数小于9，加一即可
             digits[i]++;
             return digits;
         } else if (i > 0 && digits[i] === 9) {
+            // 当前数为9，且不是最高位，置零
             digits[i] = 0;
         } else if (i === 0 && digits[i] === 9) {
-            digits.length++;
-            digits.fill(0);
-            digits[0] = 1;
+            // 当前数为9，且是最高位，置零，并多一个最高位1
+            digits[i] = 0;
+            digits.unshift(1);
             return digits;
         }
     }
 };
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
