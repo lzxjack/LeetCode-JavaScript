e829526e3f29ed6627963e494024ebe3085f88d8
Jack <965555169@qq.com>
2021-10-30 09:14:50 +0800

    update

---
+++ 位运算、技巧/260. 只出现一次的数字 III.js
@@ -0,0 +1,25 @@
+const singleNumber = nums => {
+    let temp = 0;
+    const len = nums.length;
+    for (let i = 0; i < len; i++) {
+        temp = temp ^ nums[i];
+    }
+    console.log(temp);
+    // 此时temp是两个不同的数异或的结果
+    // 寻找k，k是temp最低位为1、其余位是0的二进制数
+    let k = 1;
+    while ((temp & k) === 0) k = k << 1;
+
+    let [num1, num2] = [0, 0];
+    for (let i = 0; i < len; i++) {
+        // 分组，目的是将两个不同的数分开
+        if (nums[i] & k) {
+            num1 = num1 ^ nums[i];
+        } else {
+            num2 = num2 ^ nums[i];
+        }
+    }
+    return [num1, num2];
+};
+
+console.log(singleNumber([1, 2, 1, 3, 2, 5]));
