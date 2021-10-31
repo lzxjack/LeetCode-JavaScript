b867fd8a900571db8607e17aa61b3ae086a799b8
Jack <965555169@qq.com>
2021-10-31 08:53:31 +0800

    update

---
+++ 500. 键盘行.js
@@ -0,0 +1,18 @@
+const findWords = words => {
+    const keys = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+    const res = [];
+    // 遍历三行键盘
+    keys.forEach(key => {
+        // 满足以下条件的单词放入res
+        res.push(
+            // 从words筛选出每个字母都在当前行的单词，并展开
+            ...words.filter(word => {
+                return word
+                    .toLowerCase()
+                    .split('')
+                    .every(char => key.indexOf(char) !== -1);
+            })
+        );
+    });
+    return res;
+};
