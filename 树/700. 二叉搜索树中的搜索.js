9263cde6d9c7bca11731b5a7a65dcc828b6d6485
Jack <965555169@qq.com>
2021-11-26 08:31:42 +0800

    update

--- 树/700. 二叉搜索树中的搜索.js
+++ 树/700. 二叉搜索树中的搜索.js
@@ -1,7 +1,18 @@
+// const searchBST = (root, val) => {
+//     while (root) {
+//         if (root.val === val) return root;
+//         root = root.val > val ? root.left : root.right;
+//     }
+//     return null;
+// };
+
 const searchBST = (root, val) => {
-    while (root) {
-        if (root.val === val) return root;
-        root = root.val > val ? root.left : root.right;
+    if (!root) return null;
+    if (root.val > val) {
+        return searchBST(root.left, val);
+    } else if (root.val < val) {
+        return searchBST(root.right, val);
+    } else {
+        return root;
     }
-    return null;
 };
