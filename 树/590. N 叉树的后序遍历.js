5fe71f97be343d5e496a493f49ead04d12ffc78c
飞鸟 <965555169@qq.com>
2022-03-12 09:43:51 +0800

    update

--- 树/590. N 叉树的后序遍历.js
+++ 树/590. N 叉树的后序遍历.js
@@ -1,16 +1,32 @@
+// 迭代
 const postorder = root => {
-    if (!root) return [];
-    const res = [];
-    // 根节点入栈
-    const stack = [root];
-    // 栈还有值的时候
-    while (stack.length) {
-        // 栈顶元素出栈
-        const n = stack.pop();
-        // 如果栈顶元素有子树，子树依次入栈
-        n.children && stack.push(...n.children);
-        // 将栈顶元素推入res队列
-        res.unshift(n.val);
-    }
-    return res;
+  if (!root) return [];
+  const res = [];
+  // 根节点入栈
+  const stack = [root];
+  // 栈还有值的时候
+  while (stack.length) {
+    // 栈顶元素出栈
+    const n = stack.pop();
+    // 如果栈顶元素有子树，子树依次入栈
+    stack.push(...n.children);
+    // 将栈顶元素推入res队列
+    res.unshift(n.val);
+  }
+  return res;
 };
+
+// 递归
+// const postorder = root => {
+//   if (!root) return [];
+//   const res = [];
+
+//   const post = root => {
+//     if (!root) return;
+//     root.children.forEach(node => post(node));
+//     res.push(root.val);
+//   };
+
+//   post(root);
+//   return res;
+// };
