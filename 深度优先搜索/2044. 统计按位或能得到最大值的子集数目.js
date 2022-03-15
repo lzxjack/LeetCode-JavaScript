ffd249a8f13c04e8ab7465fdb41539f5de8ac0b6
飞鸟 <965555169@qq.com>
2022-03-15 08:20:52 +0800

    update

--- 深度优先搜索/2044. 统计按位或能得到最大值的子集数目.js
+++ 深度优先搜索/2044. 统计按位或能得到最大值的子集数目.js
@@ -1,19 +1,22 @@
 const countMaxOrSubsets = nums => {
-    let [max, count] = [0, 0];
-    const len = nums.length;
-    // start是起点
-    // orNum是 按位或 的值
-    const dfs = (start, orNum) => {
-        if (orNum === max) count++;
-        if (orNum > max) {
-            max = orNum;
-            count = 1;
-        }
-        for (let i = start; i < len; i++) {
-            // 给下一级传入orNum | nums[i]
-            dfs(i + 1, orNum | nums[i]);
-        }
-    };
-    dfs(0, 0);
-    return count;
+  let [max, count] = [0, 0];
+  const len = nums.length;
+
+  // start是起点
+  // orNum是 按位或 的值
+  const dfs = (start, orNum) => {
+    if (orNum === max) {
+      count++;
+    } else if (orNum > max) {
+      max = orNum;
+      count = 1;
+    }
+    for (let i = start; i < len; i++) {
+      // 给下一级传入orNum | nums[i]
+      dfs(i + 1, orNum | nums[i]);
+    }
+  };
+
+  dfs(0, 0);
+  return count;
 };
