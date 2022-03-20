4ea5f568e7793f585dec75921ac0d07c5d25824e
飞鸟 <965555169@qq.com>
2022-03-21 07:52:03 +0800

    update

--- 树/653. 两数之和 IV - 输入 BST.js
+++ 树/653. 两数之和 IV - 输入 BST.js
@@ -1,11 +1,16 @@
-// 函数传入一个set
-const findTarget = (node, k, set = new Set()) => {
-    // 递归到尽头，返回false
+const findTarget = (root, k) => {
+  const set = new Set();
+
+  const dfs = node => {
+    // 递归出口
     if (!node) return false;
-    // 如果set里有相应的数，返回true
+    // 找到对应的值，返回true
     if (set.has(k - node.val)) return true;
-    // 没有，就登记一下
+    // 没找到，将自己添加进去
     set.add(node.val);
-    // 递归遍历左右子树
-    return findTarget(node.left, k, set) || findTarget(node.right, k, set);
+    // 左右子树中一个找到即可
+    return dfs(node.left) || dfs(node.right);
+  };
+
+  return dfs(root);
 };
