4edb7204ce188b13df6eeded73bc09f670065a5b
飞鸟 <965555169@qq.com>
2022-03-21 08:46:58 +0800

    update

---
+++ 贪心/135. 分发糖果.js
@@ -0,0 +1,18 @@
+const candy = ratings => {
+  const len = ratings.length;
+  const candies = new Array(len).fill(1);
+
+  for (let i = 1; i < len; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  for (let i = len - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+    }
+  }
+
+  return candies.reduce((a, b) => a + b);
+};
