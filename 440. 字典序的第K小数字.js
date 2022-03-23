37fef4f993203ad0429c59b788b610adf48dcd5a
飞鸟 <965555169@qq.com>
2022-03-23 08:13:54 +0800

    update

---
+++ 440. 字典序的第K小数字.js
@@ -0,0 +1,22 @@
+const findKthNumber = (n, k) => {
+  const getCount = (prefix, n) => {
+    let count = 0;
+    for (let cur = prefix, next = prefix + 1; cur <= n; cur *= 10, next *= 10)
+      count += Math.min(next, n + 1) - cur;
+    return count;
+  };
+
+  let p = 1;
+  let prefix = 1;
+  while (p < k) {
+    let count = getCount(prefix, n);
+    if (p + count > k) {
+      prefix *= 10;
+      p++;
+    } else if (p + count <= k) {
+      prefix++;
+      p += count;
+    }
+  }
+  return prefix;
+};
