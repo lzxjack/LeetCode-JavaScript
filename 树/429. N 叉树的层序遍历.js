ac19e8aef6b43d1abb87283e543f0fadda700582
飞鸟 <965555169@qq.com>
2022-04-08 08:38:00 +0800

    update

--- 树/429. N 叉树的层序遍历.js
+++ 树/429. N 叉树的层序遍历.js
@@ -1,22 +1,18 @@
 const levelOrder = root => {
-    if (!root) return [];
-    // 根节点入队
-    const q = [root];
-    // 返回值
-    const res = [];
-    while (q.length) {
-        // 当前层的数量
-        let len = q.length;
-        // res中放入一个装当前层的空数组
-        res.push([]);
-        while (len--) {
-            // 从队列中，将当前层的元素推入res的尾巴数组中
-            const n = q.shift();
-            res[res.length - 1].push(n.val);
-            for (const item of n.children) {
-                item && q.push(item);
-            }
-        }
+  if (!root) return [];
+  const res = [];
+  const queue = [root];
+
+  while (queue.length) {
+    let len = queue.length;
+    const temp = [];
+    while (len--) {
+      const n = queue.shift();
+      temp.push(n.val);
+      n.children.forEach(node => queue.push(node));
     }
-    return res;
+    res.push(temp);
+  }
+
+  return res;
 };
