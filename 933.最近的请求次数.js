5b3acd3086c89f86fed6f44ed04ee28a19dd363d
飞鸟 <965555169@qq.com>
2022-05-06 08:52:25 +0800

    update

--- 933.最近的请求次数.js
+++ 933.最近的请求次数.js
@@ -1,11 +1,12 @@
-var RecentCounter = function() {
+class RecentCounter {
+  constructor() {
     this.q = [];
-};
-
-RecentCounter.prototype.ping = function(t) {
+  }
+  ping(t) {
     this.q.push(t);
     while (this.q[0] < t - 3000) {
-        this.q.shift();
+      this.q.shift();
     }
     return this.q.length;
-};
+  }
+}
