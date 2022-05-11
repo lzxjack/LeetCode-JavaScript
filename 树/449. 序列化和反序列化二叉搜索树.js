74d02d3bf7663d44d811b38a6d13e5b2a5c42769
飞鸟 <965555169@qq.com>
2022-05-11 09:31:52 +0800

    update

---
+++ 树/449. 序列化和反序列化二叉搜索树.js
@@ -0,0 +1,38 @@
+const serialize = root => {
+  // 先序遍历的结果
+  const preOrderRes = [];
+
+  const preOrder = root => {
+    if (!root) return;
+    preOrderRes.push(root.val);
+    preOrder(root.left);
+    preOrder(root.right);
+  };
+
+  preOrder(root);
+
+  // 因为是二叉搜索树，排序后就是中序遍历的结果
+  const inOrderRes = [...preOrderRes].sort((a, b) => a - b);
+  // 返回先序遍历、中序遍历
+  return [preOrderRes, inOrderRes];
+};
+
+// 从先序遍历、中序遍历中，返回二叉树
+const deserialize = ([preOrderRes, inOrderRes]) => {
+  if (!preOrderRes.length) return null;
+
+  const rootVal = preOrderRes[0];
+  const index = inOrderRes.indexOf(rootVal);
+
+  const preLeft = preOrderRes.slice(1, index + 1);
+  const preRight = preOrderRes.slice(index + 1);
+
+  const inLeft = inOrderRes.slice(0, index);
+  const inRight = inOrderRes.slice(index + 1);
+
+  const node = new TreeNode(rootVal);
+  node.left = deserialize([preLeft, inLeft]);
+  node.right = deserialize([preRight, inRight]);
+
+  return node;
+};
