3b2bb108eae3cddf28afcaf3ff7bd08512f7b961
lzxjack <965555169@qq.com>
2022-06-01 16:28:00 +0800

    update

--- 473. 火柴拼正方形.js
+++ 473. 火柴拼正方形.js
@@ -1,21 +1,25 @@
-const makesquare = nums => {
-  if (nums.length < 4) return false;
+const makesquare = matchsticks => {
+  const totalLen = matchsticks.reduce((a, b) => a + b);
+  if (totalLen % 4) return false;
 
-  let total = nums.reduce((i, x) => (x += i));
-  if (total % 4) return false;
+  matchsticks.sort((a, b) => b - a);
+  const len = Math.floor(totalLen / 4);
+  const edges = new Array(4).fill(0);
 
-  nums.sort((a, b) => b - a);
-  const SIDE = total / 4;
-  if (nums[0] > SIDE) return false;
-
-  let edges = [0, 0, 0, 0];
-  const dfs = i => {
-    if (i === nums.length) return true;
-    for (let k = 0; k < 4; k++) {
-      if (edges[k] + nums[i] > SIDE || (k && edges[k] === edges[k - 1])) continue;
-      edges[k] += nums[i];
-      if (dfs(i + 1)) return true;
-      edges[k] -= nums[i];
+  const dfs = index => {
+    if (index === matchsticks.length) {
+      // 所有火柴被放置，可以构成正方形
+      return true;
+    }
+    // 遍历四条边
+    for (let i = 0; i < edges.length; i++) {
+      // 当前边放入一个火柴
+      edges[i] += matchsticks[index];
+      if (edges[i] <= len && dfs(index + 1)) {
+        return true;
+      }
+      // 上面遍历过，这根不满足，则拿走这根火柴
+      edges[i] -= matchsticks[index];
     }
     return false;
   };
