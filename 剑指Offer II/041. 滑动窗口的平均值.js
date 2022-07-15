fc6bd729d89a6788dd1a94f6b041f4e0cd51e67b
lzxjack <965555169@qq.com>
2022-07-16 01:04:51 +0800

    update

--- 剑指Offer II/041. 滑动窗口的平均值.js
+++ 剑指Offer II/041. 滑动窗口的平均值.js
@@ -1,16 +1,17 @@
 class MovingAverage {
-    constructor(size) {
-        this.queue = [];
-        this.size = size;
-        this.sum = 0;
+  constructor(size) {
+    this.queue = [];
+    this.size = size;
+    this.sum = 0;
+  }
+
+  next(val) {
+    if (this.queue.length >= this.size) {
+      this.sum -= this.queue.shift();
     }
-    next(val) {
-        if (this.queue.length >= this.size) {
-            this.sum -= this.queue.shift();
-        }
-        this.queue.push(val);
-        this.sum += val;
-        const count = this.queue.length;
-        return this.sum / count;
-    }
+    this.queue.push(val);
+    this.sum += val;
+    const count = this.queue.length;
+    return this.sum / count;
+  }
 }
